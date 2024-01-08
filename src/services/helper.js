867d442d0fb9ef0a1ad183ca057f5d0dfaba035e
Raushan <raushankp75@gmail.com>
2024-01-08 16:43:39 +0530

    Change base url and deploy

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL ='https://youtube-downloader-ucfl.onrender.com';
+export const BASE_URL ='https://youtube-downloader-ucfl.onrender.com/api';
 // export const BASE_URL='http://localhost:8000/api';
 
 export const myAxios=axios.create({
